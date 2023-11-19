7109c58f7eae64d3f89e01d4fe12d8632204cab7
Felipe Augusto <workcontact.dev@gmail.com>
2023-11-19 14:20:56 -0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Olá mundo")
+AOS.init();
+
+const dataDoEvento = new Date("Dec 20,2023 24:00:00 ")
+const timeStampDoEvento = dataDoEvento.getTime();
+
+
+const contaAsHoras = setInterval (function ()  {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs =  1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const MinutoEmMs = 1000 * 60 ;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs ); 
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / MinutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}D ${horasAteOEvento}H ${minutosAteOEvento}m ${segundosAteOEvento}`
+
+    if(distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+
+}, 1000);
+
